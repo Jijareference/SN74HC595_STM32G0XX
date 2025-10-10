d2b374e17ead8dff949e0084964f6d74fe9a12db
Jijareference <Xorpen1337@yandex.ru>
2025-10-10 18:16:39 +0500

    Create SN54HC595.c

---
+++ Src/SN54HC595.c
@@ -0,0 +1,58 @@
+#include <TN54HC595.h>
+
+#include <string.h>
+
+static GPIO_TypeDef* RCK_port;
+
+static GPIO_TypeDef* SCK_port;
+
+static GPIO_TypeDef* SI_port;
+
+static uint16_t RCK_pin;
+
+static uint16_t SCK_pin;
+
+static uint16_t SI_pin;
+
+void TN54SetParam(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin, uint8_t type){
+ if (type == RCK) {
+	 RCK_port = GPIOx;
+	 RCK_pin = GPIO_Pin;
+	 HAL_GPIO_WritePin(RCK_port, RCK_pin, GPIO_PIN_RESET);
+ }
+ else if (type == SI){
+	 SI_port = GPIOx;
+	 SI_pin = GPIO_Pin;
+	 HAL_GPIO_WritePin(SI_port, SI_pin, GPIO_PIN_RESET);
+ }
+ else if (type == SCK){
+	 SCK_port = GPIOx;
+	 SCK_pin = GPIO_Pin;
+	 HAL_GPIO_WritePin(SCK_port, SCK_pin, GPIO_PIN_RESET);
+ }
+
+}
+
+static void delay_us(uint16_t us) {
+    for(uint32_t i = 0; i < us * 10; i++) {
+        __NOP();
+    }
+}
+
+void WriteByte(uint16_t Byte){
+	for (int i = 0 ;i<8; i++){
+		if (Byte & 0b01)
+			HAL_GPIO_WritePin(SI_port, SI_pin, GPIO_PIN_SET);
+		else
+			HAL_GPIO_WritePin(SI_port, SI_pin, GPIO_PIN_RESET);
+		delay_us(DELAY);
+		HAL_GPIO_WritePin(SCK_port, SCK_pin, GPIO_PIN_SET);
+		delay_us(DELAY);
+		HAL_GPIO_WritePin(SCK_port, SCK_pin, GPIO_PIN_RESET);
+		delay_us(DELAY);
+		Byte >>= 0x01;
+	}
+	HAL_GPIO_WritePin(RCK_port, RCK_pin, GPIO_PIN_SET);
+	delay_us(DELAY);
+	HAL_GPIO_WritePin(RCK_port, RCK_pin, GPIO_PIN_RESET);
+}
