2461a43391cb58a8ca075801e37f1314e3780e2b
Jijareference <Xorpen1337@yandex.ru>
2025-10-21 16:52:07 +0500

    Update SN74HC595.c

--- Src/SN74HC595.c
+++ Src/SN74HC595.c
@@ -33,13 +33,7 @@ void SN74SetParam(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin, uint8_t type){
 
 }
 
-static void delay_us(uint16_t us) {
-    for(uint32_t i = 0; i < us * 10; i++) {
-        __NOP();
-    }
-}
-
-void WriteByte(uint16_t Byte){
+void WriteByte(uint8_t Byte){
 	HAL_GPIO_WritePin(RCK_port, RCK_pin, GPIO_PIN_RESET);
 	for (int i = 0 ;i<8; i++){
 		if (Byte & 0b80)
